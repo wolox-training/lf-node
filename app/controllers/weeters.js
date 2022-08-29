ee3c69d3f8c5fdace009664d3ffb29399cfa667a
fozzattilucas <lucas.fozzatti@accenture.com>
2022-08-29 17:22:35 -0300

    fix errors

--- app/controllers/weeters.js
+++ app/controllers/weeters.js
@@ -3,5 +3,6 @@ const phrases = require('../services/friki_phrases');
 exports.getWeeter = async (req, res) => {
   const phrase = await phrases.getPhrase();
   console.log(phrase);
+  res.status(200);
   res.send(phrase);
 };
--- app/services/friki_phrases.js
+++ app/services/friki_phrases.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
 
-exports.getPhrase = async () => {
+exports.getPhrase = async res => {
   try {
-    const data = await axios.get('https://geek-jokes.sameerkumar.website/api?format=json');
-    return data.data;
+    const apiResponse = await axios.get(process.env.API_URL_PRHASES);
+    return apiResponse.data;
   } catch (error) {
     console.error(error);
+    res.status(500).send('error getting a weet');
     return error;
   }
 };
