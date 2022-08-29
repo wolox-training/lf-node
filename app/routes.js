f58ba127ddef003386a8c9f2ffee98545b8708e0
fozzattilucas <lucas.fozzatti@accenture.com>
2022-08-29 17:22:35 -0300

    fix errors

--- app/routes.js
+++ app/routes.js
@@ -4,8 +4,5 @@ const { getWeeter } = require('./controllers/weeters');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
   app.get('/weets', getWeeter);
 };
