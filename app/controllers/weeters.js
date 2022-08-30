a0f4c9ae29645deda3518f5c551842ca30586b5d
fozzattilucas <lucas.fozzatti@accenture.com>
2022-08-30 10:09:36 -0300

    join send and status

--- app/controllers/weeters.js
+++ app/controllers/weeters.js
@@ -3,6 +3,5 @@ const phrases = require('../services/friki_phrases');
 exports.getWeeter = async (req, res) => {
   const phrase = await phrases.getPhrase();
   console.log(phrase);
-  res.status(200);
-  res.send(phrase);
+  res.status(200).send(phrase);
 };
