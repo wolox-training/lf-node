fdf797d1c22d4d45f2e42e292b2d5e14ca494334
fozzattilucas <lucas.fozzatti@accenture.com>
2022-09-19 13:21:44 -0300

    delete 2 files

--- test/mocks/bcrypt.js
+++
@@ -1,8 +0,0 @@
-jest.mock('bcrypt');
-const bcrypt = require('bcrypt');
-
-exports.resolveHashPasswordMock = password => bcrypt.hash.mockResolvedValue(password);
-exports.resolveComparePasswordMock = result => {
-  console.log(result);
-  return bcrypt.compare.mockResolvedValue(result);
-};
