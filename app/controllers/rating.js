d235953aa17d7bab74f9587bf65069aaa45b72c2
fozzattilucas <lucas.fozzatti@accenture.com>
2022-09-29 16:42:51 -0300

    fix: validations

--- app/controllers/rating.js
+++ app/controllers/rating.js
@@ -5,13 +5,16 @@ exports.createRating = async (req, res) => {
   try {
     const request = { weetId: req.params.id, userId: req.id };
     const qualify = req.body;
+    if (!qualify.score) {
+      return res.status(HTTP_CODES.BAD_REQUEST).json({ message: error.rateNotSend });
+    }
     const rate = await findRate(request);
     const rateObject = Object.assign(request, qualify);
     if (!rate) {
-      createRate(rateObject);
+      await createRate(rateObject);
       return res.status(HTTP_CODES.CREATED).json({ message: success.created, rate: rateObject });
     }
-    updateRate(rateObject);
+    await updateRate(rateObject);
     return res.status(HTTP_CODES.CREATED).json({ message: success.updated });
   } catch {
     return res.status(HTTP_CODES.INTERNAL_ERROR).json({ message: error.rateNotFound });
