63d7914e30ec7b69c3edb485c1d00d95c11b5fe3
fozzattilucas <lucas.fozzatti@accenture.com>
2022-10-06 16:21:18 -0300

    weet rate errors

--- app/models/weets.js
+++ app/models/weets.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId',
         target_keys: 'id'
       });
-      Weets.hasMany(models.Raiting, {
+      Weets.hasMany(models.Rating, {
         foreignKey: 'id'
       });
     }
